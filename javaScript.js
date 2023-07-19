d0e4cf6985f1089c23fdee620ee4ed8bc3bfc9a6
Elyas <Elyas.shamal2@gmail.com>
2023-07-18 23:06:06 -0400

    display the diferent charecter

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,7 @@
 let charecterBtn = document.getElementById("get-Characters");
+let randomCharecter = Math.round(Math.random() * 80);
 charecterBtn.addEventListener("click", () => {
-  fetch("https://www.swapi.tech/api/people/1")
+  fetch(`https://www.swapi.tech/api/people/${randomCharecter}`)
     .then((res) => res.json())
     .then((data) => console.log(data));
 });
